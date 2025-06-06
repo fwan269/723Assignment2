820372275bd9a225bf5335a1ffdb2fc99346b467
fwan269 <fwan269@aucklanduni.ac.nz>
2025-06-06 13:07:45 +1200

    adjusting files for type functions and cleaned up fsm

--- assignment2/files/cruisecontrol_data.c
+++ assignment2/files/cruisecontrol_data.c
@@ -6,29 +6,21 @@
 #define PEDAL_MIN 3.0
 
 typedef enum {
-    CRUISE_OFF,
-    CRUISE_ON,
-    CRUISE_STDBY,
-    CRUISE_DISABLE
+    OFF,
+    ON,
+    STDBY,
+    DISABLE
 } CruiseState;
+#include <stdbool.h>
 
-typedef struct {
-    bool On;
-    bool Off;
-    bool Resume;
-    bool Set;
-    bool QuickDecel;
-    bool QuickAccel;
-    float Accel;   // Accelerator pedal sensor
-    float Brake;   // Brake pedal sensor
-    float Speed;   // Vehicle speed
-} CruiseInput;
 
-typedef struct {
-    float CruiseSpeed;
-    float ThrottleCmd;
-    CruiseState State;
-} CruiseOutput;
+//Page 12 lab 1
+
+// void BoolArray ( BoolArray∗ , BoolArray );
+// 	int eq_BoolArray ( BoolArray , BoolArray);
+// 	char∗ BoolArray_to_text(BoolArray);
+// 	void text_to_BoolArray(BoolArray∗, char∗); 
+// int check_BoolArray (char∗) ;
 
 
 bool isBrakePressed(float brake) {
@@ -89,73 +81,75 @@ float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed)
 	return saturateThrottle(proportionalAction + integralAction, &saturate);
 }
 
-void CruiseControl(CruiseInput input, CruiseOutput* output, bool* isGoingOn) {
+void CruiseControl(bool On, bool Off, bool Resume, bool Set, bool QuickDecel, bool QuickAccel,
+				   float Accel, float Brake, float Speed, float CruiseSpeed, 
+				   float ThrottleCmd, CruiseState cruiseState, bool isGoingOn) {
 	float cruiseLast = 0;
 
-	switch(output->State){
-		case CRUISE_OFF:
-		if(input.On){
-			output->State = CRUISE_ON;
-			*isGoingOn = true;
-			output->CruiseSpeed = input.Speed;
-			cruiseLast = output->CruiseSpeed;
+	switch(cruiseState){
+		case OFF:
+		if(On){
+			cruiseState = ON;
+			isGoingOn = true;
+			CruiseSpeed = Speed;
+			cruiseLast = CruiseSpeed;
 		} else {
 			//manual operation
 		}
 		break;
 
-		case CRUISE_ON:
-		if(input.Off){
-			output->State = CRUISE_OFF;
-		} else if (isBrakePressed(input.Brake)){
-			output->State = CRUISE_STDBY;
-		} else if (isAccelPressed(input.Accel)){
-			output->State = CRUISE_DISABLE;
+		case ON:
+		if(Off){
+			cruiseState = OFF;
+		} else if (isBrakePressed(Brake)){
+			cruiseState = STDBY;
+		} else if (isAccelPressed(Accel)){
+			cruiseState = DISABLE;
 		} else {
-			if(input.Set){
-				if(input.Set>SPEED_MAX){
-					output->CruiseSpeed=SPEED_MAX;
-				} else if(input.Set<SPEED_MIN){
-					output->CruiseSpeed=SPEED_MIN;
+			if(Set){
+				if(Set>SPEED_MAX){
+					CruiseSpeed=SPEED_MAX;
+				} else if(Set<SPEED_MIN){
+					CruiseSpeed=SPEED_MIN;
 				} else {
-					output->CruiseSpeed=input.Speed;
+					CruiseSpeed=Speed;
 				}
-			} else if(input.QuickAccel){
-				output->CruiseSpeed += SPEED_INC;
-				if(output->CruiseSpeed>SPEED_MAX){
-					output->State = CRUISE_DISABLE;
+			} else if(QuickAccel){
+				CruiseSpeed += SPEED_INC;
+				if(CruiseSpeed>SPEED_MAX){
+					cruiseState = DISABLE;
 				}
-				cruiseLast = output->CruiseSpeed;
-			} else if(input.QuickDecel){
-				output->CruiseSpeed -= SPEED_INC;
-				if(output->CruiseSpeed<SPEED_MIN){
-					output->State = CRUISE_DISABLE;
+				cruiseLast = CruiseSpeed;
+			} else if(QuickDecel){
+				CruiseSpeed -= SPEED_INC;
+				if(CruiseSpeed<SPEED_MIN){
+					cruiseState = DISABLE;
 				}
-				cruiseLast = output->CruiseSpeed;
+				cruiseLast = CruiseSpeed;
 			}
-			output->ThrottleCmd = regulateThrottle(*isGoingOn,output->CruiseSpeed,input.Speed);
-			*isGoingOn=false;
+			ThrottleCmd = regulateThrottle(isGoingOn,CruiseSpeed,Speed);
+			isGoingOn=false;
 		}
 		break;
 
-		case CRUISE_DISABLE:
-		if(input.Off){
-			output->State = CRUISE_OFF;
-		} else if(!(isAccelPressed(input.Accel)) || (SPEED_MIN<input.Speed<SPEED_MAX)){
-			output->State = CRUISE_ON;
-			output->CruiseSpeed = cruiseLast;
+		case DISABLE:
+		if(Off){
+			cruiseState = OFF;
+		} else if(!(isAccelPressed(Accel)) || (SPEED_MIN<Speed<SPEED_MAX)){
+			cruiseState = ON;
+			CruiseSpeed = cruiseLast;
 		}
 		break;
 
-		case CRUISE_STDBY:
-		if(input.Off){
-			output->State = CRUISE_OFF;
-		}else if(input.Resume){
-			if(isAccelPressed(input.Accel) || !(SPEED_MIN<input.Speed<SPEED_MAX)){
-				output->State = CRUISE_DISABLE;
+		case STDBY:
+		if(Off){
+			cruiseState = OFF;
+		}else if(Resume){
+			if(isAccelPressed(Accel) || !(SPEED_MIN<Speed<SPEED_MAX)){
+				cruiseState = DISABLE;
 			}else{
-				output->State = CRUISE_ON;
-				output->CruiseSpeed = cruiseLast;
+				cruiseState = ON;
+				CruiseSpeed = cruiseLast;
 			}
 		}
 		break;
@@ -163,11 +157,23 @@ void CruiseControl(CruiseInput input, CruiseOutput* output, bool* isGoingOn) {
 }
 
 int main(){
-	CruiseInput input = {false, true, false, false, false, false, 0, 0, 0};
-	CruiseOutput output = {0, 0, CRUISE_OFF};
+
+	bool On = false;
+	bool Off = true;
+	bool Resume = false;
+	bool Set = false;
+	bool QuickDecel = false;
+	bool QuickAccel = false;
+	float Accel = 0.0;   // Accelerator pedal sensor
+	float Brake = 0.0;   // Brake pedal sensor
+	float Speed = 0.0;   // Vehicle speed
+	float CruiseSpeed = 0.0;
+	float ThrottleCmd = 0.0;
+	CruiseState cruiseState = OFF;
+
 	bool isGoingOn = false;
 
 	while (1){
-		CruiseControl(input, &output, &isGoingOn);
+		CruiseControl(On, Off, Resume, Set, QuickDecel, QuickAccel, Accel, Brake, Speed, CruiseSpeed, ThrottleCmd, cruiseState, isGoingOn);
 	}
 }
